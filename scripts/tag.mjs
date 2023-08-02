6ac823ebcb13281a8220787d190a7791eb625917
igorskyflyer <igor.dvlpr@gmail.com>
2023-08-02 17:33:16 +0200

    Automate version signing

---
+++ scripts/tag.mjs
@@ -0,0 +1,16 @@
+import { execSync } from 'child_process'
+import { readFileSync, writeFileSync } from 'fs'
+import { exit } from 'process'
+
+try {
+  const packageFile = readFileSync('./package.json', { encoding: 'utf-8' })
+  const packageInfo = JSON.parse(packageFile)
+  const commit = execSync('git rev-parse --short=7 HEAD').toString().trim()
+
+  writeFileSync('./version', JSON.stringify({ cli: packageInfo.version, adbt: packageInfo.uses.adbt, commit }))
+  console.log('✅ Successfully tagged the release.')
+  exit(0)
+} catch {
+  console.error("❌ Couldn't tag the release")
+  exit(1)
+}
